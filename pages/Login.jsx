3b04d62adcfa59fca694afab0d5c44fbacc0a0f1
Enzo Eluan <119829559+enzoeluan@users.noreply.github.com>
2023-05-23 23:19:25 -0300

    first commit

---
+++ pages/Login.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { SafeAreaView, View, Text } from "react-native/types";
+
+export default function Login() {
+    <SafeAreaView>
+        <Text>Login</Text>
+    </SafeAreaView>
+}
